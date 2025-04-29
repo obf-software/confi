5e3284eb7c717741f19db232efcb19d984019b86
worgho2 <otavio.baziewicz.filho@gmail.com>
2025-04-29 14:41:04 -0300

    chore: add base path config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/confi/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  const basePath = env.VITE_BASE_PATH || '/';
+
+  return {
+    base: basePath,
+    plugins: [react()],
+  };
 });
