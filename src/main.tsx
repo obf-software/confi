eef6c1eb932cfb4c0e7006444fc2eec142cb202b
worgho2 <otavio.baziewicz.filho@gmail.com>
2025-05-01 17:13:45 -0300

    feat: Add typescript rules to eslint

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from './provider.tsx';
 
 import '@fontsource/open-sauce-one/index.css';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider />
