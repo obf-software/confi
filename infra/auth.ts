10a72cc8b73c2d68cf67e5115c52854cde07f262
worgho2 <otavio.baziewicz.filho@gmail.com>
2025-09-03 02:47:02 -0300

    feat(auth): enhance email confirmation message with app URL and additional instructions
    - Updated the email confirmation message to include the app URL for user convenience.
    - Improved the message formatting and added a warning about sharing the confirmation code.
    - Revised the email subject for clarity and branding consistency.

--- infra/auth.ts
+++ infra/auth.ts
@@ -1,3 +1,7 @@
+import { env } from './env';
+
+const appUrl = `https://${env.CLOUDFLARE_DOMAIN_NAME}`;
+
 const userPool = new sst.aws.CognitoUserPool('AuthUserPool', {
   transform: {
     userPool: {
@@ -5,8 +9,19 @@ const userPool = new sst.aws.CognitoUserPool('AuthUserPool', {
     },
   },
   verify: {
-    emailMessage: 'Seu código de confirmação é {####}',
-    emailSubject: 'Confirme sua conta no Confi',
+    emailMessage: `
+Your confirmation code is {####}.
+<br />
+Enter the code in the app (${appUrl}) to confirm your account.
+<br />
+<br />
+<b>Never share this code with anyone.</b>
+<br />
+<br />
+<b>Regards,
+<br />
+<b>Confi Team</b>`,
+    emailSubject: 'Confi - Confirm your account',
   },
 });
 
